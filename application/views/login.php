28018e0d57ee5c569b169b227409fab3074a452c
Fernando Handal <fernando.handal@usach.cl>
2018-04-14 12:21:44 -0300

    change login template

--- application/views/login.php
+++ application/views/login.php
@@ -1,178 +1,152 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<title>Iniciar sesión</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="lib/login-theme/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/css/util.css">
-	<link rel="stylesheet" type="text/css" href="lib/login-theme/css/main.css">
-<!--===============================================================================================-->
-</head>
-<body>
-	
-	<div class="limiter">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="container-login100">
-					<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
-						<?php
-						$attributes = array("class" => "form-horizontal", "id" => "loginform", "name" => "loginform");
-						echo form_open("login_profesor/index", $attributes);?>
-						<form class="login100-form validate-form flex-sb flex-w" accept-charset="UTF-8" action="" role="form" method="post">
-							<span class="login100-form-title p-b-32">
-								Ingreso profesor
-							</span>
 
-							<span class="txt1 p-b-11">
-								Correo electrónico
-							</span>
-							<div class="wrap-input100 validate-input m-b-36" data-validate = "Ingresa un correo">
-								<input class="input100" type="email" name="txt_username" id="txt_username" value="<?php echo set_value('txt_username'); ?>" required="true">
-								<span class="focus-input100"></span>
-							</div>
-							
-							<span class="txt1 p-b-11">
-								Contraseña
-							</span>
-							<div class="wrap-input100 validate-input m-b-12" data-validate = "Ingresa una contraseña">
-								<span class="btn-show-pass">
-									<i class="fa fa-eye"></i>
-								</span>
-								<input class="input100" type="password" name="txt_password" id="txt_password" value="<?php echo set_value('txt_password'); ?>" required="true">
-								<span class="focus-input100"></span>
-							</div>
-							
-							<div class="flex-sb-m w-full p-b-48">
-								<div class="contact100-form-checkbox">
-									<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-									<label class="label-checkbox100" for="ckb1">
-										Recordar
-									</label>
-								</div>
+    <head>
 
-								<div>
-									<a href="#" class="txt3">
-										¿Olvidaste contraseña?
-									</a>
-								</div>
-							</div>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Iniciar sesión</title>
 
-							<div class="container-login100-form-btn">
-								<button class="login100-form-btn" id="btn_login" name="btn_login" type="submit" value="Login">
-									Ingresar
-								</button>
-							</div>
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="lib/logintheme/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="lib/logintheme/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="lib/logintheme/css/form-elements.css">
+        <link rel="stylesheet" href="lib/logintheme/css/style.css">
 
-						</form>
-						<?php echo form_close(); ?>
-						<div class="row"> <p style="color:white;">.</p> </div>
-						<?php echo $this->session->flashdata('msg_profesor'); ?>
-					</div>
-				</div>
-			</div>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-			<div class="col-md-6">
-				<div class="container-login100">
-					<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
-						<?php
-						$attributes = array("class" => "form-horizontal", "id" => "loginform", "name" => "loginform");
-						echo form_open("login_alumno/index", $attributes);?>
-						<form class="login100-form validate-form flex-sb flex-w" accept-charset="UTF-8" action="" role="form" method="post">
-							<span class="login100-form-title p-b-32">
-								Ingreso alumno
-							</span>
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="lib/logintheme/assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="lib/logintheme/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="lib/logintheme/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="lib/logintheme/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="lib/logintheme/ico/apple-touch-icon-57-precomposed.png">
 
-							<span class="txt1 p-b-11">
-								Correo electrónico
-							</span>
-							<div class="wrap-input100 validate-input m-b-36" data-validate = "Ingresa un correo">
-								<input class="input100" type="email" name="txt_username" id="txt_username" value="<?php echo set_value('txt_username'); ?>" required="true">
-								<span class="focus-input100"></span>
-							</div>
-							
-							<span class="txt1 p-b-11">
-								Contraseña
-							</span>
-							<div class="wrap-input100 validate-input m-b-12" data-validate = "Ingresa una contraseña">
-								<span class="btn-show-pass">
-									<i class="fa fa-eye"></i>
-								</span>
-								<input class="input100" type="password" name="txt_password" id="txt_password" value="<?php echo set_value('txt_password'); ?>" required="true">
-								<span class="focus-input100"></span>
-							</div>
-							
-							<div class="flex-sb-m w-full p-b-48">
-								<div class="contact100-form-checkbox">
-									<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-									<label class="label-checkbox100" for="ckb1">
-										Recordar
-									</label>
-								</div>
+    </head>
 
-								<div>
-									<a href="#" class="txt3">
-										¿Olvidaste contraseña?
-									</a>
-								</div>
-							</div>
+    <body>
 
-							<div class="container-login100-form-btn">
-								<button class="login100-form-btn" id="btn_login" name="btn_login" type="submit" value="Login">
-									Ingresar
-								</button>
-							</div>
+        <!-- Top content -->
+        <div class="top-content">
+        	
+            <div class="inner-bg">
+                <div class="container">
+                	
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text">
+                            <h1>MEMORIA 2018</h1>
+                            <div class="description">
+                            	<p>
+	                            	
+                            	</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-sm-5">
+                        	
+                        	<div class="form-box">
+	                        	<div class="form-top">
+	                        		<div class="form-top-left">
+	                        			<h3>INGRESO PROFESOR</h3>
+	                            		<p>
+                                            Ingresa tu correo electrónico y contraseña
+                                        </p>
+	                        		</div>
+	                        		<div class="form-top-right">
+	                        			<i class="fa fa-key"></i>
+	                        		</div>
+	                            </div>
+	                            <div class="form-bottom">
 
-						</form>
-						<?php echo form_close(); ?>
-						<div class="row"> <p style="color:white;">.</p> </div>
-						<?php echo $this->session->flashdata('msg_alumno'); ?>
-					</div>
-				</div>
-			</div>
-		</div>
+                                    <?php
+                                    $attributes = array("class" => "form-horizontal", "id" => "loginform", "name" => "loginform");
+                                    echo form_open("login_profesor/index", $attributes);?>
+				                    <form role="form" action="" method="post" class="login-form">
+				                    	<div class="form-group">
+				                    		<label class="sr-only" for="txt_username">Correo electrónico</label>
+				                        	<input type="text" name="txt_username" placeholder="Correo electrónico" id="txt_username" value="<?php echo set_value('txt_username'); ?>" required="true" class="form-username form-control">
+				                        </div>
+				                        <div class="form-group">
+				                        	<label class="sr-only" for="txt_password">Contraseña</label>
+				                        	<input class="form-password form-control"  placeholder="Contraseña" type="password" name="txt_password" id="txt_password" value="<?php echo set_value('txt_password'); ?>" required="true">
+				                        </div>
+				                        <button id="btn_login" name="btn_login" type="submit" value="Login" class="btn">Ingresar</button>
+				                    </form>
+                                    <?php echo form_close(); ?>
+                                    <div class="row"> <p style="color:white;">.</p> </div>
+                                    <?php echo $this->session->flashdata('msg_profesor'); ?>
+			                    </div>
+		                    </div>
+		                
+	                        
+                        </div>
+                        
+                        <div class="col-sm-1 middle-border"></div>
+                        <div class="col-sm-1"></div>
+                        	
+                        <div class="col-sm-5">
+                        	
+                            <div class="form-box">
+                                <div class="form-top">
+                                    <div class="form-top-left">
+                                        <h3>INGRESO ALUMNO</h3>
+                                        <p>
+                                            Ingresa tu correo electrónico y contraseña
+                                        </p>
+                                    </div>
+                                    <div class="form-top-right">
+                                        <i class="fa fa-key"></i>
+                                    </div>
+                                </div>
+                                <div class="form-bottom">
+                                    <?php
+                                    $attributes = array("class" => "form-horizontal", "id" => "loginform", "name" => "loginform");
+                                    echo form_open("login_alumno/index", $attributes);?>
 
-	</div>
+                                    <form role="form" action="" method="post" class="login-form">
+                                        <div class="form-group">
+                                            <label class="sr-only" for="txt_username">Correo electrónico</label>
+                                            <input class="form-username form-control" placeholder="Correo electrónico" type="text" name="txt_username" id="txt_username" value="<?php echo set_value('txt_username'); ?>" required="true">
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="sr-only" for="txt_password">Contraseña</label>
+                                            <input class="form-password form-control" type="password" placeholder="Contraseña" name="txt_password" id="txt_password" value="<?php echo set_value('txt_password'); ?>" required="true">
+                                        </div>
+                                        <button class="btn" id="btn_login" name="btn_login" type="submit" value="Login">Ingresar</button>
+                                    </form>
+                                    <?php echo form_close(); ?>
+                                    <div class="row"> <p style="color:white;">.</p> </div>
+                                    <?php echo $this->session->flashdata('msg_alumno'); ?>
+                                </div>
+                            </div>
+                        	
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
 
 
-	
+        <!-- Javascript -->
+        <script src="lib/logintheme/js/jquery-1.11.1.min.js"></script>
+        <script src="lib/logintheme/bootstrap/js/bootstrap.min.js"></script>
+        <script src="lib/logintheme/js/scripts.js"></script>
+        
+        <!--[if lt IE 10]>
+            <script src="assets/js/placeholder.js"></script>
+        <![endif]-->
 
-	<div id="dropDownSelect1"></div>
-	
-<!--===============================================================================================-->
-	<script src="lib/login-theme/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="lib/login-theme/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="lib/login-theme/vendor/bootstrap/js/popper.js"></script>
-	<script src="lib/login-theme/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="lib/login-theme/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="lib/login-theme/vendor/daterangepicker/moment.min.js"></script>
-	<script src="lib/login-theme/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="lib/login-theme/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="lib/login-theme/js/main.js"></script>
+    </body>
 
-</body>
 </html>
