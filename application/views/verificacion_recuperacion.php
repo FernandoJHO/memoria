5524a841c368ffd1ac05bc0972f19c23d76f2373
Fernando Handal <fernando.handal@usach.cl>
2018-06-30 00:21:38 -0400

    fix

--- application/views/verificacion_recuperacion.php
+++ application/views/verificacion_recuperacion.php
@@ -39,7 +39,7 @@
                 width: 100%;
                 height: 100%;
                 z-index: 9999;
-                background: url('lib/img/loader.gif') 50% 50% no-repeat rgb(249,249,249);
+                background: url('<?php echo base_url();?>lib/img/loader.gif') 50% 50% no-repeat rgb(249,249,249);
                 opacity: .8;
             }
         </style>
